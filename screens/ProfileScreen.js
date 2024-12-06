860327b8a0b2eb91f77856bb6c3b5e4bc2df367f
hamnakayani <hamnakayani2004@gmail.com>
2024-12-06 07:24:50 +0500

    Profilescreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,32 +1,80 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Image, ScrollView } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons'; 
+import React, { useEffect, useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Alert
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-const ProfileScreen = () => {
-  const healthMetrics = [
-    { icon: '🌡️', title: '56', subtitle: 'Heart Rate' },
-    { icon: '💧', title: '120/80', subtitle: 'Blood Pressure' },
-    { icon: '📏', title: "5", subtitle: 'Height' },
-    { icon: '⚖️', title: '135', subtitle: 'Weight' },
-    { icon: '❤️', title: '125', subtitle: 'Pulse Rate' },
-    { icon: '🔥', title: '135', subtitle: 'Calories' },
-  ];
+const ProfileScreen = ({ navigation }) => {
+  const [userName, setUserName] = useState('');
+  const [userRole, setUserRole] = useState('');
+  const [loading, setLoading] = useState(true);
 
-  const quickActions = [
-    { icon: '✉️', title: 'Message' },
-    { icon: '🔔', title: 'Notification' },
-    { icon: '💊', title: 'Pills Reminder' },
-    { icon: '📝', title: 'Our Blogs' },
-  ];
+  // Fetch user profile from API
+  const fetchProfile = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+
+      const response = await fetch('http://192.168.18.25:3001/auth/fetchProfile', {
+        method: 'GET',
+        headers: {
+        token:token
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setUserName(data.name || 'Unknown User');
+        setUserRole(data.role || 'Unknown Role');
+      } else {
+        Alert.alert('Error', 'Failed to fetch profile. Please try again.');
+        navigation.navigate('Login');
+      }
+    } catch (error) {
+      console.error('Failed to fetch profile:', error);
+      Alert.alert('Error', 'An error occurred while fetching profile.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProfile();
+  }, []);
+
+  // Logout functionality
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem('token');
+//      Alert.alert('Logout', 'You have been logged out.', [
+//        { text: 'OK', onPress: () => navigation.navigate('Login') },
+//      ]);
+    } catch (error) {
+      console.error('Failed to logout:', error);
+      Alert.alert('Error', 'Failed to logout. Please try again.');
+    }
+  };
 
   const menuItems = [
-    { title: 'Basic Information', icon: 'person' },
+    { title: 'basic Information', icon: 'person' },
     { title: 'Change Password', icon: 'lock' },
     { title: 'Settings', icon: 'settings' },
     { title: 'Legal Information', icon: 'description' },
-    { title: 'Send Feedback', icon: 'feedback' },
-    { title: 'Logout', icon: 'logout' },
+    {
+      title: 'Send Feedback',
+      icon: 'feedback',
+      onPress: () => navigation.navigate('Feedback')
+    },
+    { title: 'Logout', icon: 'logout', onPress: handleLogout },
   ];
+
 
   return (
     <ScrollView style={styles.container}>
@@ -38,8 +86,12 @@ const ProfileScreen = () => {
             style={styles.profileImage}
           />
           <View style={styles.headerText}>
-            <Text style={styles.userName}>Joshua Fullulah</Text>
-            <Text style={styles.userLocation}>Bennettsville, TX</Text>
+            <Text style={styles.userName}>
+              {loading ? 'Loading...' : userName}
+            </Text>
+            <Text style={styles.userRole}>
+              {loading ? 'Loading...' : userRole}
+            </Text>
           </View>
         </View>
         <TouchableOpacity style={styles.myAccountButton}>
@@ -47,31 +99,14 @@ const ProfileScreen = () => {
         </TouchableOpacity>
       </View>
 
-      {/* Health Metrics Grid */}
-      <View style={styles.metricsContainer}>
-        {healthMetrics.map((metric, index) => (
-          <View key={index} style={styles.metricItem}>
-            <Text style={styles.metricIcon}>{metric.icon}</Text>
-            <Text style={styles.metricValue}>{metric.title}</Text>
-            <Text style={styles.metricLabel}>{metric.subtitle}</Text>
-          </View>
-        ))}
-      </View>
-
-      {/* Quick Actions Grid */}
-      <View style={styles.quickActionsContainer}>
-        {quickActions.map((action, index) => (
-          <TouchableOpacity key={index} style={styles.actionItem}>
-            <Text style={styles.actionIcon}>{action.icon}</Text>
-            <Text style={styles.actionTitle}>{action.title}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-
       {/* Menu Items */}
       <View style={styles.menuContainer}>
         {menuItems.map((item, index) => (
-          <TouchableOpacity key={index} style={styles.menuItem}>
+          <TouchableOpacity
+            key={index}
+            style={styles.menuItem}
+            onPress={item.onPress ? item.onPress : () => {}}
+          >
             <View style={styles.menuItemContent}>
               <Icon name={item.icon} size={24} color="#4A4A4A" />
               <Text style={styles.menuItemText}>{item.title}</Text>
@@ -90,7 +125,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
   },
   header: {
-    backgroundColor: '#2B547E',
+    backgroundColor: '#008080',
     padding: 20,
     paddingTop: 40,
     borderBottomLeftRadius: 30,
@@ -120,12 +155,12 @@ const styles = StyleSheet.create({
     fontSize: 22,
     fontWeight: 'bold',
   },
-  userLocation: {
+  userRole: {
     color: '#E0E0E0',
     fontSize: 14,
   },
   myAccountButton: {
-    backgroundColor: '#3A668B',
+    backgroundColor: '#008080',
     paddingHorizontal: 20,
     paddingVertical: 12,
     borderRadius: 25,
@@ -141,78 +176,6 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 14,
     fontWeight: 'bold',
-  },
-  metricsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 10,
-    backgroundColor: 'white',
-    marginTop: 20,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 4,
-    elevation: 4,
-  },
-  metricItem: {
-    width: '33.33%',
-    padding: 15,
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: '#e0e0e0',
-    borderRightWidth: 1,
-    borderRightColor: '#e0e0e0',
-  },
-  metricIcon: {
-    fontSize: 30,
-    marginBottom: 8,
-    color: '#2B547E',
-  },
-  metricValue: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#4A4A4A',
-  },
-  metricLabel: {
-    fontSize: 12,
-    color: '#757575',
-  },
-  quickActionsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    padding: 10,
-    backgroundColor: 'white',
-    marginTop: 20,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.15,
-    shadowRadius: 4,
-    elevation: 4,
-  },
-  actionItem: {
-    width: '50%',
-    padding: 15,
-    alignItems: 'center',
-    borderWidth: 0.5,
-    borderColor: '#E0E0E0',
-    borderRadius: 12,
-    marginBottom: 15,
-    backgroundColor: '#f9f9f9',
-    transition: 'background-color 0.3s ease',
-  },
-  actionItemHovered: {
-    backgroundColor: '#d1d1d1',
-  },
-  actionIcon: {
-    fontSize: 30,
-    marginBottom: 5,
-    color: '#2B547E',
-  },
-  actionTitle: {
-    fontSize: 14,
-    color: '#4A4A4A',
   },
   menuContainer: {
     backgroundColor: 'white',
@@ -233,10 +196,6 @@ const styles = StyleSheet.create({
     borderBottomColor: '#E0E0E0',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    transition: 'background-color 0.3s ease',
-  },
-  menuItemHovered: {
-    backgroundColor: '#f2f2f2',
   },
   menuItemContent: {
     flexDirection: 'row',
